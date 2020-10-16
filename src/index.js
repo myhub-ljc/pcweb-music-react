e5a44ded15ac5c21587f0d00ed5d55c9de5586ba
myhub-ljc <2321170474@qq.com>
2020-10-16 18:07:43 +0800

    css样式重置以及个人重置的样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//对重置的样式做一个导入
+import "./assets/css/reset.css";
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
