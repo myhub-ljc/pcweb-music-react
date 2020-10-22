29d0dd6669582f5dc35fa0647746b6cb4772df35
myhub-ljc <2321170474@qq.com>
2020-10-23 00:04:46 +0800

    header完成(input框未加入icons)

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,27 +1,58 @@
+//第三方
 import React, { memo } from 'react';
+
+import { HeaderLists } from '@/common/local-data.js';
 
 //组件
 import { NavLink } from 'react-router-dom';
+// import { SearchOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
 
 import {
   HeaderWrapper,
-  ContentLeft,
-  ContentRight
+  HeaderLeft,
+  HeaderRight
 } from './styled';
 
 export default memo(function JCAppHeader() {
+
+  const ShowSelectItem = ((item, index) => {
+    if(index < 3) {
+      return (
+        <NavLink to={item.link} exact={true}>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
+      )
+    } else {
+      return <a href={item.link}>{item.title}</a>
+    }
+  })
+
   return (
     <HeaderWrapper>
       <div className="content wrap-v1">
-        <ContentLeft>
-          <a href="#/" className="logo sprite_01"></a>
-        </ContentLeft>
-        <ContentRight>right</ContentRight>
+        <HeaderLeft>
+          <a href="#/" className="logo sprite_01">网易云音乐</a>
+          <div className="select-list">
+            {
+              HeaderLists.map((item, index) => {
+                return (
+                  <div key={item.title} className="select-item">
+                    {ShowSelectItem(item, index)}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input className="search" placeholder="音乐/视频/电台/用户"/>
+          <div className="center">创作者中心</div>
+          <div>登陆</div>
+        </HeaderRight>
       </div>
       <div className="divider"></div>
-      {/* <NavLink to='/'>发现</NavLink>
-      <NavLink to='/mine'>我的音乐</NavLink>
-      <NavLink to='/friend'>朋友</NavLink> */}
     </HeaderWrapper>
   )
 })
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import JCDiscover from '@/pages/discover';
 import JCMine from '@/pages/mine';
 import JCFriend from '@/pages/friend';
 
+//记住别又把component写成components了(崩溃了)
 const routes = [
   {
     path: '/',
